8cd340bb20a3b29594b28c2cd69cabc88a814104
Josh Tait <josh.tait@visualsoft.co.uk>
2024-07-03 20:12:54 +0100

    flesh out tests with data providers testing all requirements to meet a given classes implementation of the provided interface

--- tests/Unit/StrTest.php
+++ tests/Unit/StrTest.php
@@ -1,29 +1,46 @@
 <?php
 
-it('checks if a string is an anagram', function () {
-    $anagram    = 'anna';
-    $notAnagram = 'banana';
-
-    $service = new Str();
-
-    $this->assertTrue($service->isAnagram($anagram, 'anna'));
-    $this->assertFalse($service->isAnagram($anagram, $notAnagram));
+beforeEach(function () {
+    $this->service = new Str();
 });
 
-it('checks if a string is a palindrome', function () {
-    $palindrome    = 'anna';
-    $notPalindrome = 'banana';
+it('checks for palindromes when given', function (string $subject, bool $result) {
+    expect($this->service->isAnagram($subject))->toBe($result);
+})->with([
+    'anna'                           => ['anna', true],
+    'banana'                         => ['banana', false],
+    'You should give Josh a job'     => ['You should give Josh a job', false],
+    'a sequence of numbers'          => ['1337', false],
+    'a another sequence of numbers'  => ['9009', true],
+    'a given sequence of characters' => ['!*abc1235321cba', false],
+    'another sequence of characters' => ['!*abc123321cba*!', true]
+]);
 
-    $service = new Str();
+it(
+    'checks for anagrams when given',
+    function (
+        string $subject,
+        string $comparison,
+        bool $result
+    ) {
+        expect($this->service->isPalindrome($subject))->toBeTrue();
+    })->with([
+    'glean'                                                        => ['glean', 'angel', true],
+    'listen'                                                       => ['listen', 'silent', true],
+    'a sequence of numbers'                                        => ['1337', '7331', true],
+    'another sequence of numbers'                                  => ['9009', '90091', false],
+    'a phrase'                                                     => ['the eyes', 'they see', true],
+    'another phrase'                                               => [
+        'Why of course you can, old sport', 'The Great Gatsby', false
+    ],
+    'a phrase that is really close to being an anagram but isn\'t' => ['thee eyes', 'they see', false]
+]);
 
-    $this->assertTrue($service->isPalindrome($palindrome));
-    $this->assertFalse($service->isPalindrome($notPalindrome));
-});
-
-it('checks if a string is a pangram', function () {
-    $pangram = 'The quick brown fox jumps over the lazy dog';
-
-    $service = new Str();
-    expect($service->isPangram($pangram))->toBeTrue();
-});
-
+it('checks for pangrams when given', function (string $phrase, bool $result) {
+    expect($this->service->isPangram($phrase))->toBe($result);
+})->with([
+    'an actual angram' => ['A wizard’s job is to vex chumps quickly in fog.', true],
+    'a random phrase'  => [
+        'The problem with having an open mind is that people insist on putting things inside of it', false
+    ]
+]);
